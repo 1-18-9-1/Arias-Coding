b1649c7c4946fbd4327cb0314d07e09d59d3b484
1-18-9-1 <peeplesaria@gmail.com>
2024-11-09 12:30:45 -0800

    learning.js

---
+++ learning.js
@@ -0,0 +1,19 @@
+{
+     let branch= "#"
+    for (let count = branch; count < branch+branch; count++)
+        console.log(count);
+    for (let count = branch+branch; count < branch+branch+branch; count++)
+        console.log(count); 
+    for (let count = branch+branch+branch; count < branch+branch+branch+branch; count++)
+        console.log(count);
+    for (let count = branch+branch+branch+branch; count < branch+branch+branch+branch+branch; count++)
+        console.log(count);
+    for (let count = branch+branch+branch+branch+branch; count < branch+branch+branch+branch+branch+branch; count++)
+        console.log(count);
+    for (let count = branch+branch+branch+branch+branch+branch; count < branch+branch+branch+branch+branch+branch+branch; count++)
+        console.log(count);
+    for (let count = branch+branch+branch+branch+branch+branch+branch; count < branch+branch+branch+branch+branch+branch+branch+branch; count++)
+        console.log(count);
+    for (let count = branch; count < branch+branch; count++)
+        console.log(count);
+}
